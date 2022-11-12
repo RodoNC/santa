ce69148107d32f46ddce3ab918ff69fe485bf281
RodoNC <rodolfoncalip@gmail.com>
2022-11-12 18:24:03 -0500

    finished movement

--- Santa.gd
+++ Santa.gd
@@ -24,7 +24,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#animation
-	print(velocity.x)
 	if velocity.length() < 200:
 		if velocity.x < 100:
 			player.play("idle")
